4fe15f3f696dac3702f3b5532b6471ab3bd1db84
Tomas Vondra <tv@fuzzy.cz>
2026-01-25 17:57:59 +0100

    rework the API to work like postgresql-hll
    and tdigest 2.0.0

--- test/sql/copy.sql
+++ test/sql/copy.sql
@@ -1,3 +1,12 @@
+\set ECHO none
+
+-- disable the notices for the create script (shell types etc.)
+SET client_min_messages = 'WARNING';
+\i sql/ddsketch--1.0.0.sql
+\i sql/ddsketch--1.0.0--1.0.1.sql
+\i sql/ddsketch--1.0.1--1.0.2-dev.sql
+SET client_min_messages = 'NOTICE';
+
 CREATE TABLE ddsketch_src (id int, s ddsketch);
 CREATE TABLE ddsketch_dst (id int, s ddsketch);
 
