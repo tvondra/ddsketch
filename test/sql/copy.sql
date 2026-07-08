831b988be17b05ff649d4c1a8088a7e127b52738
Tomas Vondra <tv@fuzzy.cz>
2026-07-08 16:51:57 +0200

    Introduce a separate _init regression test
    This merely installs the extension objects, so that the other tests can
    simply assume everything is already set up.

--- test/sql/copy.sql
+++ test/sql/copy.sql
@@ -1,11 +1,3 @@
-\set ECHO none
-
--- disable the notices for the create script (shell types etc.)
-SET client_min_messages = 'WARNING';
-\i sql/ddsketch--1.0.0.sql
-\i sql/ddsketch--1.0.0--1.0.1.sql
-SET client_min_messages = 'NOTICE';
-
 CREATE TABLE ddsketch_src (id int, s ddsketch);
 CREATE TABLE ddsketch_dst (id int, s ddsketch);
 
